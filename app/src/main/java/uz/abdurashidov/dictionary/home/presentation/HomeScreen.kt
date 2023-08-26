5d59e42e448b0622ec5c786d2148afd3b210c0a6
Saydullo <abdurashidovsaydullo35@gmail.com>
2023-08-26 23:29:54 +0500

    Add font

--- app/src/main/java/uz/abdurashidov/dictionary/home/presentation/HomeScreen.kt
+++ app/src/main/java/uz/abdurashidov/dictionary/home/presentation/HomeScreen.kt
@@ -1,8 +1,68 @@
 package uz.abdurashidov.dictionary.home.presentation
 
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import kotlinx.coroutines.flow.collectLatest
+import uz.abdurashidov.dictionary.common.UiEvents
+import uz.abdurashidov.dictionary.ui.theme.DictionaryTheme
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    dictionaryViewModel: DictionaryViewModel = hiltViewModel()
+) {
+    val scaffoldState = rememberScaffoldState()
 
+    LaunchedEffect(key1 = true) {
+        dictionaryViewModel.eventFlow.collectLatest { event ->
+            when (event) {
+                is UiEvents.SnackBarEvent -> {
+                    scaffoldState.snackbarHostState.showSnackbar(event.message)
+                }
+            }
+        }
+    }
+
+    val definitionUiState = dictionaryViewModel.definitionUiState.collectAsState().value
+    val typeWord = dictionaryViewModel.typeWord.value
+    val definitions =
+        if (definitionUiState.definition?.isNotEmpty() == true) definitionUiState.definition[0].meanings
+            ?: emptyList()
+        else emptyList()
+
+
+    DictionaryTheme {
+        Scaffold(
+            scaffoldState = scaffoldState,
+            topBar = {
+                TopAppBar(
+                    title ={
+                        Text(
+                            text = buildAnnotatedString {
+                                withStyle(
+                                    style = SpanStyle(
+                                        fontSize = 16.sp,
+                                        fontFamily = FontFamily(Font)
+                                    )
+                                )
+                            }
+                        )
+                    }
+                )
+            }
+        ) {
+
+        }
+    }
 }
